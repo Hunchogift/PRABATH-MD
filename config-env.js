d9433b395db8944ec0272929b6ce16bfec53ecb7
Hunchogift <giftmasosote@gmail.com>
2024-06-13 01:08:10 +0200

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  process.env.BOT_NUMBER,
+    SESSION_ID:  process.env.SESSION_ID,PRABATH-MD~d6FQiDQI#7s2mWPRJW8n7Fmx2haR5V1k3VwHLWhbMQwfJTUhhH5g  
+    BOT_NUMBER:  process.env.BOT_NUMBER,263773168961
     GITHUB_USERNAME: username,
     GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
 };
